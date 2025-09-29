6b442d2966298bd486af2d44f429b392dc9ebaaf
İsmet Şafak <safakismet@gmail.com>
2025-09-29 11:06:04 +0300

    add icons

--- components/raceControls.vue
+++ components/raceControls.vue
@@ -2,18 +2,24 @@
   <div>
     <div class="button-group">
       <v-button
-        text="Next Race"
-        type="primary"
-        size="sm"
-        :disabled="(store.currentRound >= store.totalRounds) || (!currentRoundAllResults?.finished)"
-        @click="$emit('nextRace')"
-      />
-      <v-button
         text="Start Race"
         type="primary-dark"
         size="sm"
+        right-icon="play"
+        icon-size="muk-md"
+        is-stroked
         :disabled="(Object.keys(currentRoundAllResults).length !== 0)"
         @click="$emit('startRace')"
+      />
+      <v-button
+        text="Next Race"
+        type="primary"
+        size="sm"
+        right-icon="arrow-right"
+        icon-size="muk-md"
+        is-stroked
+        :disabled="(store.currentRound >= store.totalRounds) || (!currentRoundAllResults?.finished)"
+        @click="$emit('nextRace')"
       />
       <v-button
         v-if="(store.currentRound >= store.totalRounds)"
